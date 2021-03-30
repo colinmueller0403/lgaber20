fa42d793073fe2478bd90343388a3679430b0139
CMueller7 <colin.mueller@patelhs.org>
2021-03-30 21:22:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) 
+  {
+    
+    String greeting1 = "Greetings"; // literal string!
+    
+    String greeting2 = new String("Hello"); // new string!
+    
+    String firstname = new String("Colin"); 
+    
+    String lastname = " Mueller"; 
+    
+    String word = "Multiplication"; 
+    
+    String a = "1"; 
+    
+    String b = "4"; 
+    
+    System.out.println(greeting2 + " " + firstname); 
+    
+    System.out.println("The word multiplication has a word length of " + word.length
+    ()); // word length
+    
+    System.out.println("a + b = " + a + b); // Concatenate
+    
+    System.out.println("Thank you so much for your time and I hope you enjoy the rest of your day!");   
+
+  }
+}
